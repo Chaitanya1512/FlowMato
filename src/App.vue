c98c16fdb7d45f342ccd552386f793f23e126122
Chaitanya <thakur.chaitanya25@gmail.com>
2024-07-01 19:36:54 +0530

    simple pomodoro timer

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col col-sm-6 col-offset-sm-3">
+          <PomodoroTimer />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PomodoroTimer from "./components/PomodoroTimer.vue";
+
+export default {
+  name: "App",
+  components: {
+    PomodoroTimer,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap");
+@import "./assets/styles/PomodoroTimer.css";
+
+#app {
+  font-family: "Work Sans", Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container {
+  width: 100%;
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 0 15px;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+}
+
+.col {
+  position: relative;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+.col-sm-6 {
+  flex: 0 0 50%;
+  max-width: 50%;
+}
+
+.col-offset-sm-3 {
+  margin-left: 25%;
+}
+</style>
---
+++ src/components/PomodoroTimer.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="card">
+    <div class="tab-container">
+      <div class="slider-bar">
+        <div class="slider-switch" :style="switchStyle"></div>
+        <div
+          class="slider-tab"
+          v-for="session in sessions"
+          :key="session.name"
+          @click="changeSession(session.name)"
+          :class="{ active: currentSession === session.name }"
+        >
+          {{ session.name }}
+        </div>
+      </div>
+    </div>
+
+    <div class="tabs-window">
+      <div class="card-content">
+        <div class="card-text">
+          <h1 class="time">{{ minute }}:{{ second }}</h1>
+          <div class="button-group">
+            <button v-if="!inProgress" @click="start" class="btn btn-primary">
+              Start
+            </button>
+            <button v-else @click="stop" class="btn btn-error">Stop</button>
+            <button
+              @click="reset(this.currentSession)"
+              :disabled="inProgress"
+              class="btn"
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      totalDuration: 25 * 60,
+      currentSession: "Pomodoro",
+      interval: null,
+      inProgress: false,
+      sessions: [
+        { name: "Pomodoro", duration: 25 * 60 },
+        { name: "Short Break", duration: 5 * 60 },
+        { name: "Long Break", duration: 15 * 60 },
+      ],
+    };
+  },
+  computed: {
+    switchStyle() {
+      const activeIndex = this.sessions.findIndex(
+        (session) => session.name == this.currentSession
+      );
+      return {
+        transform: `translateX(${activeIndex * 97.5}%)`,
+      };
+    },
+    minute() {
+      const minutes = Math.floor(this.totalDuration / 60);
+      return minutes < 10 ? "0" + minutes : minutes;
+    },
+    second() {
+      const seconds = this.totalDuration % 60;
+      return seconds < 10 ? "0" + seconds : seconds;
+    },
+  },
+  methods: {
+    start() {
+      this.stop();
+      this.inProgress = true;
+      this.interval = setInterval(() => {
+        if (--this.totalDuration <= 0) {
+          this.stop();
+          return;
+        }
+      }, 1000);
+    },
+    stop() {
+      clearInterval(this.interval);
+      this.inProgress = false;
+    },
+    reset(sessionName) {
+      this.stop();
+      const session = this.sessions.find(
+        (session) => session.name === sessionName
+      );
+      this.totalDuration = session.duration;
+    },
+    changeSession(sessionName) {
+      this.currentSession = sessionName;
+      this.reset(sessionName);
+    },
+  },
+};
+</script>
